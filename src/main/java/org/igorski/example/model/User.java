02877eb84fccf85d49911525c2bb8aa7b55891ca
Igor Stojanovski <igorce@outlook.com>
2019-02-23 22:05:53 +0100

    feature: Introduce lombok

--- src/main/java/org/igorski/example/model/User.java
+++ src/main/java/org/igorski/example/model/User.java
@@ -1,16 +1,18 @@
 package org.igorski.example.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- * Created by Igor Stojanovski.
- * Date: 3/14/2018
- * Time: 10:07 PM
- */
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "app_user")
 public class User {
@@ -23,54 +25,4 @@ public class User {
     private String password;
     private String name;
     private String surname;
-
-    public Long getId() {
-
-        return id;
-    }
-
-    public void setId(Long id) {
-
-        this.id = id;
-    }
-
-    public String getUsername() {
-
-        return username;
-    }
-
-    public void setUsername(String username) {
-
-        this.username = username;
-    }
-
-    public String getPassword() {
-
-        return password;
-    }
-
-    public void setPassword(String password) {
-
-        this.password = password;
-    }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public void setName(String name) {
-
-        this.name = name;
-    }
-
-    public String getSurname() {
-
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-
-        this.surname = surname;
-    }
 }
